21a0385cbd495ba0a5880169fddbd8cc1843851b
Nakati Mat <matheus.nakati.b2@gmail.com>
2022-07-19 04:51:52 -0300

    Upload_Low_Changes_V.0.1.0

--- System_Creation/Assets/Scripts/Enemy/EnemyCommunA.I.cs
+++ System_Creation/Assets/Scripts/Enemy/EnemyCommunA.I.cs
@@ -1,33 +1,33 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.AI;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEngine.AI;
 
-public class EnemyCommunA.I : MonoBehaviour
-{
-    public NavMeshAgent navMeshAgent;
-    public float startWaitTime = 4;
-    public float timeToRotate = 2;
-    public float speedWalk = 6;
-    public float speedRun = 9;
+//public class EnemyCommunA.I : MonoBehaviour
+//{
+//    public NavMeshAgent navMeshAgent;
+//    public float startWaitTime = 4;
+//    public float timeToRotate = 2;
+//    public float speedWalk = 6;
+//    public float speedRun = 9;
 
-    public float viewRadius = 15;
-    public float viewAngle = 90;
-    public LayerMask playerMask;
-    public LayerMask obstacleMask;
-    public float meshResolution = 1.0f;
-    public int edgeIterations = 4;
-    public float edgeDistance = 0.5f;
+//    public float viewRadius = 15;
+//    public float viewAngle = 90;
+//    public LayerMask playerMask;
+//    public LayerMask obstacleMask;
+//    public float meshResolution = 1.0f;
+//    public int edgeIterations = 4;
+//    public float edgeDistance = 0.5f;
 
 
-    void Start()
-    {
+//    void Start()
+//    {
         
-    }
+//    }
 
     
-    void Update()
-    {
+//    void Update()
+//    {
         
-    }
-}
+//    }
+//}
